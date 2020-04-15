e7c3ab93d7f962d6e55889a29ac20cd640ad8a7e
Denis Souza <denis.tracoleal@gmail.com>
2020-04-15 10:33:07 -0300

    #18.2 Consumindo dados da API do google

--- api_google_calendar_php/quickstart.php
+++ api_google_calendar_php/quickstart.php
@@ -1,10 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-// if (php_sapi_name() != 'cli') {
-//     throw new Exception('This application must be run on the command line.');
-// }
-
 /**
  * Returns an authorized API client.
  * @return Google_Client the authorized client object
@@ -74,158 +70,217 @@ $optParams = array(
 $results = $service->events->listEvents($calendarId, $optParams);
 $events = $results->getItems();
 
-// if (empty($events)) {
-//     print "No upcoming events found.\n";
-// } else {
-//    // print "Upcoming events:\n";
-//     foreach ($events as $event) {
-//         $start = $event->start->dateTime;
-//         if (empty($start)) {
-//             $start = $event->start->date;
-//         }
-//         // Descrição
-//         //print_r($event->getDescription());
-//         $descricao = $event->getDescription();
-//         // Titulo ou assunto
-//        // print_r($event->getSummary());
-//         $titulo = $event->getSummary();
-//         //print_r($titulo);
-//         print_r(`<p>Titulo: <b></b>."$titulo".</p><br>`);
-//         echo `<p>Titulo: <b></b>."$titulo".</p><br>`;
-//         // Criação
-//        // print_r($event->getCreated());
-//         //printf("%s (%s)\n", $event->getSummary(), $start);
-        
-        
-        
-//     }
-// }
-
 ?>
 
 
 
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <title>Banco de Idéias</title>
-        <base href="/">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    </head>
+<head>
+    <meta charset="utf-8">
+    <title>Calendario Google</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
 
-    <body>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-            </script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-            </script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-            </script>
-
-
-
-
-        <div class="container">
-
-            <div class="container col-md-10">
-                <?php
-
-                
-
-                if (empty($events)) {
-                    print "No upcoming events found.\n";
-                } else {
-                   // print "Upcoming events:\n";
-                    foreach ($events as $event) {
-                        $start = $event->start->dateTime;
-                        if (empty($start)) {
-                            $start = $event->start->date;
-                        }
-                        // Descrição
-                        //print_r($event->getDescription());
-                        $descricao = $event->getDescription();
-                        // Titulo ou assunto
-                       // print_r($event->getSummary());
-                        $titulo = $event->getSummary();
-                        
-                        //print_r($titulo);
-                        //print_r(`<p>Titulo: <b></b>."$titulo".</p><br>`);
-                        //echo `<p>Titulo: <b></b>."$titulo".</p><br>`;
-                        // Criação
-                       // print_r($event->getCreated());
-                        //printf("%s (%s)\n", $event->getSummary(), $start);
-                        
-                        
-                        
-                    }
-
-                    echo "
-                    <div class='form-row'>
-                        <div class='form-group col-md-6'>
-                            <label for='titulo'><b>Titulo: </b></label>
-                                <input type='text' class='form-control' name='titulo' value='$titulo'>
-                        </div>
-                    </div> 
-                    
-                    <div class='form-row'>
-                        <div class='form-group col-md-6'>
-                        <label for='descricao'><b>Descrição: </b></label>
-                            <textarea name='' id='' cols='30' rows='10' disabled>$descricao</textarea>
-                        </div>
-                    </div> 
-                    ";
-                }
-
-                ?>
-                <br>
-            </div>
-
-            <div class="container col-md-10">
-                <label for="">Agenda</label>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="titulo"><b>Titulo: </b></label>
-                            <input type="text" class="form-control" name="titulo" value='$titulo'>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="data"><b>Data: </b></label>
-                            <input type="date" class="form-control" name="data" value="">
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="hora"><b>Hora: </b></label>
-                            <input type="time" class="form-control" name="hora" value="">
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="descricao"><b>Descrição: </b></label>
-                            <textarea name="" id="" cols="30" rows="10" disabled></textarea>
-                        
-                    </div>
-                </div>
-            </div>
-            
-
-            <div class="container col-md-10">
-                <br>
-
-                
-            </div>
+<body>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
 
 
 
+
+    <div class="container">
+
+        <div class="container col-md-10">
 
         </div>
 
-    </body>
+        <div class="container col-md-10">
+            
+            <table id="dtVerticalScrollExample" class="table table-striped table-bordered table-sm" cellspacing="0"
+                    width="100%">
+                    <label for="">Hoje</label>
+                    <thead>
+                        <tr>
+                            <th class="th-sm">Titulo
+                            </th>
+                            <th class="th-sm">Data
+                            </th>
+                            <th class="th-sm">Hora
+                            </th>
+                            <th class="th-sm">Descrição
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            <?php
+
+                                
+
+                                if (empty($events)) {
+                                    print "No upcoming events found.\n";
+                                } else {
+
+                                    // print "Upcoming events:\n";
+                                    foreach ($events as $event) {
+                                        $start = $event->start->dateTime;
+                                        
+                                        if (empty($start)) {
+                                            $start = $event->start->date;
+                                            
+                                        }
+                                        
+                                        // Data
+                                        //$date = str_split($start,10)[0];
+                                        $date = explode("T",$start)[0];
+                                        //Hora
+                                        $hora = explode("T",$start)[1];
+                                        // Descrição
+                                        $descricao = $event->getDescription();
+                                        // Titulo ou assunto
+                                        $titulo = $event->getSummary();
+                                        //print_r($titulo);
+                                        //print_r(`<p>Titulo: <b></b>."$titulo".</p><br>`);
+                                        //echo `<p>Titulo: <b></b>."$titulo".</p><br>`;
+                                        // Criação
+                                        //print_r($event->getCreated());
+                                        //printf("%s (%s)\n", $event->getSummary(), $start);
+                                        if($date == date('yy-m-d') )
+                                        {
+                                            echo "
+                                            <tr>
+                                                <td>
+                                                    $titulo
+                                                </td>
+                                                <td>
+                                                    $date
+                                                </td>
+                                                <td>
+                                                    $hora
+                                                </td>
+                                                <td>
+                                                    $descricao
+                                                </td>
+                                            </tr>
+                                            ";
+                                        }
+                                        
+                                        
+                                    }
+
+                                    
+                                
+                                }
+
+                                ?>
+                            </tbody>
+            </table>
+                <br>
+            
+        </div>
+
+
+        <div class="container col-md-10">
+            
+            <table id="dtVerticalScrollExample" class="table table-striped table-bordered table-sm" cellspacing="0"
+                    width="100%">
+                    <label for="">Proximos Eventos</label>
+                    <thead>
+                        <tr>
+                            <th class="th-sm">Titulo
+                            </th>
+                            <th class="th-sm">Data
+                            </th>
+                            <th class="th-sm">Hora
+                            </th>
+                            <th class="th-sm">Descrição
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            <?php
+
+                                
+
+                                if (empty($events)) {
+                                    print "No upcoming events found.\n";
+                                } else {
+
+                                    // print "Upcoming events:\n";
+                                    foreach ($events as $event) {
+                                        $start = $event->start->dateTime;
+                                        
+                                        if (empty($start)) {
+                                            $start = $event->start->date;
+                                            
+                                        }
+                                        
+                                        // Data
+                                        //$date = str_split($start,10)[0];
+                                        $date = explode("T",$start)[0];
+                                        //Hora
+                                        $hora = explode("T",$start)[1];
+                                        // Descrição
+                                        $descricao = $event->getDescription();
+                                        // Titulo ou assunto
+                                        $titulo = $event->getSummary();
+                                        //print_r($titulo);
+                                        //print_r(`<p>Titulo: <b></b>."$titulo".</p><br>`);
+                                        //echo `<p>Titulo: <b></b>."$titulo".</p><br>`;
+                                        // Criação
+                                        //print_r($event->getCreated());
+                                        //printf("%s (%s)\n", $event->getSummary(), $start);
+                                        if($date != date('yy-m-d') )
+                                        {
+                                            echo "
+                                            <tr>
+                                                <td>
+                                                    $titulo
+                                                </td>
+                                                <td>
+                                                    $date
+                                                </td>
+                                                <td>
+                                                    $hora
+                                                </td>
+                                                <td>
+                                                    $descricao
+                                                </td>
+                                            </tr>
+                                            ";
+                                        }
+                                        
+                                        
+                                    }
+
+                                    
+                                
+                                }
+
+                                ?>
+                            </tbody>
+            </table>
+                <br>
+            
+        </div>
+
+        </div>
+
+
+
+
+    </div>
+
+</body>
 
 </html>
