9fc7427223fa9ca57b2163147d25e4b95317505c
Denis Souza <denis.tracoleal@gmail.com>
2020-04-17 09:49:17 -0300

    #19 Login

---
+++ login/Database/script.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `usuario` (
+  id int(11) PRIMARY KEY AUTO_INCREMENT,
+  login varchar (255),
+  senha varchar(255) NOT NULL,
+  email varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
