3f83aa7a6defd2f411aa89362661d906cdb386f0
Denis Souza <denis.tracoleal@gmail.com>
2020-05-05 17:20:43 -0300

    #22.2 Editando solução 1

--- stepform-jquery/js/formstep.js
+++ stepform-jquery/js/formstep.js
@@ -1,31 +1,42 @@
-$(function () {
 
-    var form = $("#example-form");
-    form.validate({
-        errorPlacement: function errorPlacement(error, element) { element.before(error); },
-        rules: {
-            confirm: {
-                equalTo: "#password"
-            }
+$(document).ready(function () {
+
+    //Esconde todos os passos e exibe o primeiro ao carregar a página 
+    $('.step').hide();
+    $('.step').first().show();
+
+    console.log($('#form'))
+    //Exibe no topo em qual passo estamos pela ordem da div que esta visivel
+    var passoexibido = function () {
+        var index = parseInt($(".step:visible").index());
+        if (index == 0) {
+            //Se for o primeiro passo desabilita o botão de voltar
+            $("#prev").prop('disabled', true);
+        } else if (index == (parseInt($(".step").length) - 1)) {
+            //Se for o ultimo passo desabilita o botão de avançar
+            $("#next").prop('disabled', true);
+        } else {
+            //Em outras situações os dois serão habilitados
+            $("#next").prop('disabled', false);
+            $("#prev").prop('disabled', false);
         }
+        $("#passo").html(index + 1);
+
+    };
+
+    //Executa a função ao carregar a página
+    passoexibido();
+
+    //avança para o próximo passo
+    $("#next").click(function () {
+        $(".step:visible").hide().next().show();
+        passoexibido();
     });
-    form.children("div").steps({
-        headerTag: "h3",
-        bodyTag: "section",
-        transitionEffect: "slideLeft",
-        onStepChanging: function (event, currentIndex, newIndex)
-        {
-            form.validate().settings.ignore = ":disabled,:hidden";
-            return form.valid();
-        },
-        onFinishing: function (event, currentIndex)
-        {
-            form.validate().settings.ignore = ":disabled";
-            return form.valid();
-        },
-        onFinished: function (event, currentIndex)
-        {
-            alert("Submitted!");
-        }
+
+    //retrocede para o passo anterior
+    $("#prev").click(function () {
+        $(".step:visible").hide().prev().show();
+        passoexibido();
     });
+
 });
--- stepform-jquery/processa.php
+++ stepform-jquery/processa.php
@@ -1,20 +1,20 @@
 
 <?php
 
-    $nome = $_POST['nome'];
-    $email = $_POST["email"];
-    $endereco =$_POST['end'];
-    $fone = $_POST["fone"];
-    $cep = $_POST["cep"];
-    $bairro = $_POST["bairro"];
+    // $nome = $_POST['nome'];
+    // $email = $_POST["email"];
+    // $endereco =$_POST['end'];
+    // $fone = $_POST["fone"];
+    // $cep = $_POST["cep"];
+    // $bairro = $_POST["bairro"];
 
    
-
-    if($nome == null || $email == null || $endereco == null || $fone == null || $cep == null || $bairro == null ){
-        header('Location:/prefeitura/stepform-jquery/index.php?erro=1');
-    }else{
-        header('Location:/prefeitura/stepform-jquery/');
-    }
+    print_r($_POST);
+    // if($nome == null || $email == null || $endereco == null || $fone == null || $cep == null || $bairro == null ){
+    //     header('Location:/prefeitura/stepform-jquery/solucao1.html?erro=1');
+    // }else{
+    //     header('Location:/prefeitura/stepform-jquery/solucao1.html');
+    // }
 
 
    
